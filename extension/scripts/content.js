c9d6f84845e5311b7f6515bc1e00b48fbc48d42e
E Harrison <ehharrison13@gmail.com>
2023-09-25 23:53:55 -0700

    Using background.js framework, added buttons

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,29 +1,52 @@
-// if (window.location.href.includes("nytimes.com/crosswords/game")) { 
-//     const body = document.querySelector("body");
-//     console.log(body)
-//     const content = body.querySelector(".pz-content")
-//     const game_section = content.querySelector("#crossword-container")
-//     const game_wrapper = game_section.querySelector(".pz-game-wrapper")
-//     const game_screen = game_wrapper.querySelector(".pz-game-screen")
-//     const portal_game_modal = game_screen.children[1]
-//     console.log(portal_game_modal)
-//     // const start_game_button = portal_game_modal.querySelector(".modal-system-container")
-//     //.querySelector(".xwd__modal--wrapper").querySelector(".xwd__modal--body")
-//     // console.log(start_game_button)
+console.log("HI");
+const getAlternateClue = function (clue_text) {
+  return "Testing for now lol";
+};
+const swapClue = function (text) {
+  return function () {
+    var allClues = document.querySelectorAll(".xwd__clue--text");
+    var i = 0;
+    var actualClue;
+    while (allClues[i].innerHTML !== text && i < allClues.length) {
+      i++;
+    }
+    actualClue = allClues[i];
+    parentClue = actualClue.parentElement;
+    alternateClue = parentClue.querySelector(".xwd__clue--alternate");
+    if (
+      alternateClue.style.display === "" ||
+      alternateClue.style.display === "none"
+    ) {
+      console.log("Swapping Clues!");
+      actualClue.style.display = "none";
+      alternateClue.style.display = "block";
+    } else {
+      console.log("Swapping Back!");
+      actualClue.style.display = "block";
+      alternateClue.style.display = "none";
+    }
+  };
+};
 
-
-
-//     // const game_field = game_screen.querySelector(".pz-game-field")
-//     // console.log(game_field)
-//     // const puzzle = game_field.children[0]
-//     // // querySelector("xwd__layout_container") // .querySelector("div").querySelector("article")
-//     // console.log(puzzle)
-// }
-
-const init = function() {
-    const injectElement = document.createElement("div");
-    injectElement.className = "test-element";
-    injectElement.innerHTML = "Hello!";
-    document.body.appendChild(injectElement);
+if (window.location.href.includes("nytimes.com/crosswords/game")) {
+  const clueLists = document.querySelector(".xwd__layout--cluelists");
+  const allClues = clueLists.querySelectorAll(".xwd__clue--li");
+  // Create the new clues and buttons which allow for swapping clues
+  allClues.forEach((clue) => {
+    const clueText = clue.querySelector(".xwd__clue--text").innerHTML;
+    const altClueText = getAlternateClue(clueText);
+    // Make new element that has the alternate clue
+    const alternateClue = document.createElement("span");
+    alternateClue.className = "xwd__clue--alternate";
+    alternateClue.innerHTML = altClueText;
+    clue.appendChild(alternateClue);
+    // Make button to allow for the current clue to swap with the alternate one
+    const alternateClueButton = document.createElement("button");
+    alternateClueButton.className = "xwd__clue--alternate-button";
+    alternateClueButton.innerHTML = "Show";
+    alternateClueButton.onclick = swapClue(clueText);
+    clue.appendChild(alternateClueButton);
+  });
+  // Create a modal interface warning the user that they are trying to show an easier clue
+  
 }
-init();
