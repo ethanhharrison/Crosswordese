6dedf05e152eb89144cce777e9e18d069b80d753
E Harrison <ehharrison13@gmail.com>
2023-12-07 16:59:32 -0800

    tile movement added

--- crossword.py
+++ crossword.py
@@ -24,9 +24,8 @@ class Tile:
     blocked: bool
     num_down: int
     num_across: int
-    correct_entry: str = " "
-    current_entry: str = " "
-    empty: bool = True
+    correct_entry: str = ""
+    current_entry: str = ""
     selected: bool = False
     highlighted: bool = False
     down_clue: Optional[Clue] = None
@@ -38,9 +37,9 @@ class Tile:
             self.down_clue = clue
         elif orientation == "across":
             self.across_clue = clue
-            
+
     def highlight(self, orientation: str) -> None:
-        if orientation == "across" and self.across_clue: 
+        if orientation == "across" and self.across_clue:
             self.highlighted = any([tile.selected for tile in self.across_clue.tiles])
         elif orientation == "down" and self.down_clue:
             self.highlighted = any([tile.selected for tile in self.down_clue.tiles])
@@ -57,28 +56,29 @@ class Tile:
             raise BlockedTileError("Cannot fill blocked tile")
         elif len(value) > 1:
             raise ValueError("Value is too long")
-        elif value not in string.ascii_uppercase:
+        elif value not in string.ascii_letters:
             raise ValueError("Invalid character")
         else:
-            self.current_entry = value
-            self.empty = True
+            self.current_entry = value.upper()
 
     def remove(self) -> None:
         """Removes the tile's current entry"""
-        if self.empty:
-            self.current_entry = " "
-            self.empty = False
+        self.current_entry = ""
 
     def display_border(self, padding: int, size: int, border_width: int):
         x_pos = padding + size * self.num_across
         y_pos = padding + size * self.num_down
-        black_rect = pygame.Rect(x_pos, y_pos, size, size)
-        yield pygame.Color("black"), black_rect
+        outer_rect = pygame.Rect(x_pos, y_pos, size, size)
         inner_rect = pygame.Rect(
             x_pos + border_width,
             y_pos + border_width,
             size - border_width * 2,
-            size - border_width * 2)
+            size - border_width * 2,
+        )
+        if self.blocked:
+            yield pygame.Color("black"), outer_rect
+        else:
+            yield pygame.Color(105, 105, 105), outer_rect
         if self.selected:
             yield pygame.Color(255, 217, 2), inner_rect
         elif self.highlighted:
@@ -89,7 +89,7 @@ class Tile:
     def display_current_entry(self, padding: int, size: int, font):
         x_pos = padding + size * self.num_across
         y_pos = padding + size * self.num_down
-        if self.current_entry != " ":
+        if self.current_entry:
             text = font.render(self.current_entry, True, "black")
             text_rect = text.get_rect()
             text_rect.center = (x_pos + size // 2 - 1, y_pos + size // 2 + 1)
@@ -100,7 +100,7 @@ class Tile:
         y_pos = padding + size * self.num_down
         text = font.render(str(number), True, "black")
         text_rect = text.get_rect()
-        text_rect.topleft = (x_pos + 4, y_pos + 3)
+        text_rect.topleft = (x_pos + 4, y_pos - 1)
         return text, text_rect
 
     def print_clues(self):
@@ -128,7 +128,7 @@ class Clue:
     num_down: int
     num_across: int
     tiles: list[Tile] = field(default_factory=list)
-    
+
     def connect_tiles(self, tiles: list[Tile]) -> None:
         self.tiles = tiles
 
@@ -137,11 +137,11 @@ class Clue:
         if self.tiles == []:
             raise ValueError("No tiles connected to clue")
         for value, tile in zip(self.solution, self.tiles):
-            if tile.correct_entry == " " or tile.correct_entry == value:
+            if tile.correct_entry == "" or tile.correct_entry == value:
                 tile.assign_clue(self, self.orientation)
                 tile.correct_entry = value
             else:
-                raise InvalidPuzzleError("Puzzle has conflicting clues")       
+                raise InvalidPuzzleError("Puzzle has conflicting clues")
 
     def __str__(self) -> str:
         output = f"{self.question}:\n"
@@ -159,7 +159,6 @@ class GameBoard:
         self.clues = clues
         self.tiles = self.make_tile_grid(rows, cols)
         self.selected_tile = self.get_tile(0, 0)
-        
 
     def make_tile_grid(self, rows: int, cols: int) -> list[list[Tile]]:
         """Make a tile grid for the board"""
@@ -172,9 +171,15 @@ class GameBoard:
         for clue in self.clues:
             sol_size = len(clue.solution)
             if clue.orientation == "across":
-                tiles = [self.get_tile(clue.num_down, clue.num_across + i) for i in range(sol_size)]
+                tiles = [
+                    self.get_tile(clue.num_down, clue.num_across + i)
+                    for i in range(sol_size)
+                ]
             else:
-                tiles = [self.get_tile(clue.num_down + i, clue.num_across) for i in range(sol_size)]
+                tiles = [
+                    self.get_tile(clue.num_down + i, clue.num_across)
+                    for i in range(sol_size)
+                ]
             clue.connect_tiles(tiles)
             clue.update_tile_solutions()
 
@@ -182,7 +187,7 @@ class GameBoard:
         """Make any tile with an empty correct solution a blocked tile"""
         for row in self.tiles:
             for tile in row:
-                if tile.correct_entry == " ":
+                if tile.correct_entry == "":
                     tile.blocked = True
 
     def is_complete(self) -> bool:
@@ -192,11 +197,17 @@ class GameBoard:
                 return False
         return True
 
+    def in_bounds(self, num_down, num_across):
+        if num_down < 0 or num_across < 0:
+            return False
+        elif num_down >= self.rows or num_across >= self.cols:
+            return False
+        else:
+            return True
+
     def get_tile(self, num_down: int, num_across: int) -> Tile:
         """Given the x and y coordinates, selects a tile on the board"""
-        if num_down < 0 or num_across < 0:
-            raise ValueError("Invalid tile position")
-        elif num_down >= self.rows or num_across >= self.cols:
+        if not self.in_bounds(num_down, num_across):
             raise ValueError("Invalid tile position")
         elif self.tiles[num_down][num_across].blocked:
             raise BlockedTileError("Cannot select a blocked tile")
@@ -209,20 +220,24 @@ class GameBoard:
         self.selected_tile.selected = False
         self.selected_tile = new_tile
         self.selected_tile.selected = True
-        
+
     def move(self, orientation: str, direction: int) -> None:
         num_down = self.selected_tile.num_down
         num_across = self.selected_tile.num_across
-        if orientation == "across":
-            try:
-                new_tile = self.get_tile(num_down, num_across + direction)
-                pass
-            except BlockedTileError:
+        still_moving = True
+        while still_moving:
+            if orientation == "across":
                 num_across += direction
-            self.change_selected_tile(num_down, num_across)
-        elif orientation == "down":
-            pass
-            self.change_selected_tile(num_down, num_across)
+            else:
+                num_down += direction
+            if self.in_bounds(num_down, num_across):
+                try:
+                    self.change_selected_tile(num_down, num_across)
+                    still_moving = False
+                except BaseException:
+                    pass
+            else:
+                still_moving = False
 
     def update_tile_entry(self, value: str) -> None:
         """Adds or removes value to the selected tile"""
@@ -231,12 +246,13 @@ class GameBoard:
         else:
             self.selected_tile.fill(value)
 
-    def display_board(self, screen_height: int):
+    def display_board(self, screen_height: int, border_width: int):
         padding = round(0.05 * screen_height)
         tile_size = round((0.9 * screen_height) // self.rows)
-        board_width = tile_size * self.cols + 4
-        board_height = tile_size * self.rows + 4
-        board_rect = pygame.Rect(padding - 2, padding - 2, board_width, board_height)
+        board_width = tile_size * self.cols + border_width
+        board_height = tile_size * self.rows + border_width
+        offset = padding - border_width // 2
+        board_rect = pygame.Rect(offset, offset, board_width, board_height)
         board_color = pygame.Color("black")
         return board_color, board_rect
 
@@ -245,7 +261,7 @@ class GameBoard:
             row = self.tiles[i]
             for j in range(len(row)):
                 tile = row[j]
-                tile_display = tile.display_border(padding, tile_size, 2)
+                tile_display = tile.display_border(padding, tile_size, 1)
                 text_display = tile.display_current_entry(padding, tile_size, font)
                 yield tile_display, text_display
 
@@ -258,6 +274,7 @@ class GameBoard:
                 output += str(tile)
             output += "\n"
         return output
+
 
 def create_board(rows: int, cols: int, clues: list[Clue]) -> GameBoard:
     board = GameBoard(rows, cols, clues)
