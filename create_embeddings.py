76bca876238e9bf056426543bf7974e677100d8b
E Harrison <ehharrison13@gmail.com>
2023-12-11 23:26:31 -0800

    refactoring

--- create_embeddings.py
+++ create_embeddings.py
@@ -1,16 +1,19 @@
-import config # api key
+import config  # api key
 
 from crossword_parser import get_all_QA_pairs
 from tiktoken import encoding_for_model
 import os
 import json
-import re
+import pandas as pd
 
 QA_PAIR_FOLDER = "nyt_crosswords-master"
 GPT_MODEL = "gpt-3.5-turbo"
 EMBEDDING_MODEL = "text-embedding-ada-002"
+EMBEDDING_PATH = "data/nyt_qa_requests_to_parallel_process_results.jsonl"
+SAVE_PATH = "data/qa_pairs.csv"
 MAX_TOKENS = 1600
 BATCH_SIZE = 40
+
 
 # Recursively split QA dataset to smaller sections so GPT can read it
 # see: https://cookbook.openai.com/examples/embedding_wikipedia_articles_for_search
@@ -19,84 +22,63 @@ def num_tokens(text: str, model: str = GPT_MODEL) -> int:
     encoding = encoding_for_model(model)
     return len(encoding.encode(text))
 
-def halved_by_delimiter(string: str, delimiter: str = "\n") -> list[str]:
-    """Split a string in two, on a delimiter"""
-    chunks = string.split(delimiter)
-    if len(chunks) == 1:
-        return [string, ""]
-    elif len(chunks) == 2:
-        return chunks
-    else:
-        split_idx = len(chunks) // 2
-        left = delimiter.join(chunks[:split_idx])
-        right = delimiter.join(chunks[split_idx:])
-        return [left, right]
-    
+
 def truncated_string(
-    string: str,
-    model: str,
-    max_tokens: int,
-    print_warning: bool = True
+    string: str, model: str, max_tokens: int, print_warning: bool = True
 ) -> str:
     """Truncate a string to a maximum number of tokens"""
     encoding = encoding_for_model(model)
     encoded_string = encoding.encode(string)
     truncated_string = encoding.decode(encoded_string[:max_tokens])
     if print_warning and len(encoded_string) > max_tokens:
-        print(f"Warning: Truncated string from {len(encoded_string)} tokens to {max_tokens} tokens.")
+        print(
+            f"Warning: Truncated string from {len(encoded_string)} tokens to {max_tokens} tokens."
+        )
     return truncated_string
 
-def split_string(
-    string: str,
-    max_tokens: int = 1000,
-    model: str = GPT_MODEL,
-    max_recursion: int = 15
-) -> list[str]:
-    num_tokens_in_string = num_tokens(string)
-    # if length is fine, return string
-    if num_tokens_in_string <= max_tokens:
-        return [string]
-    # if recursion hasn't found a split after X iterations, just truncate
-    elif max_recursion == 0:
-        return [truncated_string(string, model=model, max_tokens=max_tokens)]
-    # otherwise, split in half and perform recursion
-    else:
-        left, right = halved_by_delimiter(string, delimiter="\n")
-        results = []
-        for half in [left, right]:
-            half_strings = split_string(
-                half,
-                max_tokens=max_tokens,
-                model=model,
-                max_recursion=max_recursion-1
-            )
-            results.extend(half_strings)
-        if len(results) > 2000:
-            print(f"{len(string)}-long string split into {len(results)} strings.")
-        return results
-    
+
 def split_string_by_lines(
-    string: str,
-    lines_per_split: int,
-    max_tokens: int = 1000,
-    model = GPT_MODEL
+    string: str, lines_per_split: int = 5, max_tokens: int = 1000, model=GPT_MODEL
 ):
     split_string = string.split("\n")
     all_splits = []
     i = 0
     split_size = lines_per_split
     while i < len(split_string) - split_size:
-        combined_string = "\n".join(split_string[i:i+split_size])
+        combined_string = "\n".join(split_string[i : i + split_size])
         if num_tokens(combined_string) <= max_tokens:
             all_splits.append(combined_string)
             i += split_size
             split_size = lines_per_split
-        elif split_size == 1: # this should basically never happen
-            truncated = truncated_string(combined_string, model=model, max_tokens=max_tokens)
+        elif split_size == 1:  # this should basically never happen
+            truncated = truncated_string(
+                combined_string, model=model, max_tokens=max_tokens
+            )
             all_splits.append(truncated)
         else:
             split_size -= 1
     return all_splits
+
+
+def retrieve_embeddings(file_path: str = EMBEDDING_PATH) -> pd.DataFrame:
+    strings = []
+    responses = []
+    with open(file_path) as file:
+        for batch_num, line in enumerate(file):
+            try:
+                string, response = json.loads(line)
+                print(f"{batch_num}:", string["input"][0])
+                embeddings = [e["embedding"] for e in response["data"]]
+                assert len(string["input"]) == len(embeddings)
+                strings.extend(string["input"])
+                responses.extend(embeddings)
+            except BaseException as be:
+                print(be)
+                break
+    print(len(strings), len(responses))
+    df = pd.DataFrame({"text": strings, "embedding": responses})
+    return df
+
 
 def main() -> None:
     # collect data
@@ -105,10 +87,11 @@ def main() -> None:
     for clue in QA_list:
         QA_text += f"{clue.question} ({len(clue.solution)} letters): {clue.solution}\n"
     print(f"{len(QA_list)} QA pairs collected")
-        
+
     # split sections into chunks
-    # QA_strings = split_string(QA_text, max_tokens=MAX_TOKENS)
-    QA_strings = split_string_by_lines(QA_text, 20, max_tokens=MAX_TOKENS)
+    QA_strings = split_string_by_lines(
+        QA_text, lines_per_split=20, max_tokens=MAX_TOKENS
+    )
     print(f"{len(QA_list)} QA pairs split into {len(QA_strings)} strings.")
     print(num_tokens(QA_strings[0]))
 
@@ -119,10 +102,10 @@ def main() -> None:
         batch = QA_strings[batch_start:batch_end]
         QA_batches.append(batch)
         print(f"Batch {batch_start} to {batch_end-1}")
-        
+
     # save strings to jsonl file for parallel processing
     # see: https://github.com/openai/openai-cookbook/blob/main/examples/api_request_parallel_processor.py
-    filename = "data/nyt_qa_requests_to_parallel_process.jsonl"
+    filename = EMBEDDING_PATH
     jobs = [{"model": EMBEDDING_MODEL, "input": QA_batch} for QA_batch in QA_batches]
     with open(filename, "w") as f:
         for job in jobs:
@@ -132,6 +115,11 @@ def main() -> None:
 
     # run parallel processing
     os.system("sh parallel_process.sh")
+
+    # save embeddings as csv
+    df = retrieve_embeddings()
+    df.to_csv(SAVE_PATH, index=False)
+
 
 if __name__ == "__main__":
     main()
--- crossword_guesser.py
+++ crossword_guesser.py
@@ -1,36 +1,29 @@
-from sched import scheduler
-import config   # api key
+import config  # api key
 
-from create_embeddings import num_tokens
+from create_embeddings import EMBEDDING_MODEL, GPT_MODEL, SAVE_PATH, num_tokens
 from crossword import Crossword, Clue
 from copy import copy
 from scipy import spatial
 import pandas as pd
 import dask.dataframe as dd
-import dask
 import openai
-import json
 import ast
 
-GPT_MODEL = "gpt-3.5-turbo"
-EMBEDDING_MODEL = "text-embedding-ada-002"
-EMBEDDING_PATH = "data/nyt_qa_requests_to_parallel_process_results.jsonl"
-SAVE_PATH = "data/qa_pairs.csv"
 
 class BadModelError(BaseException):
     pass
 
+
 class Solver:
     """Class to represent the puzzle solver"""
-    def __init__(self, 
-        puzzle: Crossword, 
-        has_solved: bool=False, 
-        guesses: list[str]=[]
+
+    def __init__(
+        self, puzzle: Crossword, has_solved: bool = False, guesses: list[str] = []
     ) -> None:
         self.puzzle = copy(puzzle)
         self.has_solved = has_solved
         self.guesses = guesses
-        
+
     # given a crossword, solve for each tile in the puzzle
     def solve(self) -> None:
         solutions: dict[Clue, str] = {}
@@ -43,36 +36,18 @@ class Solver:
             for tile, char in zip(clue.tiles, sol):
                 tile.fill(char)
         self.has_solved = True
-    
+
     # given a clue, answer it!
     def answer_clue(self, clue: Clue) -> str:
         return ""
-        
+
     def accuracy(self) -> float:
         if self.has_solved:
             return self.puzzle.board.accuracy()
         else:
             return -1
-    
-def retrieve_embeddings(file_path: str = EMBEDDING_PATH) -> pd.DataFrame:
-    strings = []
-    responses = []
-    with open(file_path) as file:
-        for batch_num, line in enumerate(file):
-            try:
-                string, response = json.loads(line)
-                print(f"{batch_num}:", string["input"][0])
-                embeddings = [e["embedding"] for e in response["data"]]
-                assert len(string["input"]) == len(embeddings)
-                strings.extend(string["input"])
-                responses.extend(embeddings)
-            except BaseException as be:
-                print(be)
-                break
-    print(len(strings), len(responses))
-    df = pd.DataFrame({"text": strings, "embedding": responses})
-    return df
-    
+
+
 # search function which takes the query and the dataframe to rank the most similar embeddings
 # see: https://cookbook.openai.com/examples/question_answering_using_embeddings
 def strings_ranked_by_relatedness(
@@ -84,84 +59,87 @@ def strings_ranked_by_relatedness(
     """Returns a list of strings and relatedness, sorted from most related to least"""
     print("Searching for most related clues")
     query_embedding_response = openai.embeddings.create(
-        model=EMBEDDING_MODEL,
-        input=query
+        model=EMBEDDING_MODEL, input=query
     )
-    query_embedding = query_embedding_response.data[0].embedding    
-    strings_and_relatedness = [
-        (row["text"], relatedness_fn(query_embedding, row["embedding"]))
-        for i, row in df.iterrows()
-    ]
+    query_embedding = query_embedding_response.data[0].embedding
+    strings_and_relatedness = []
+    for i, row in df.iterrows():
+        relatedness = relatedness_fn(query_embedding, row["embedding"])
+        print(f"Row {i} relatedness: {relatedness}")
+        strings_and_relatedness.append((row["text"], relatedness))
     strings_and_relatedness.sort(key=lambda x: x[1], reverse=True)
     strings, relatedness = zip(*strings_and_relatedness)
     return strings[:top_n], relatedness[:top_n]
-        
-def query_message(
-    query: str,
-    df: pd.DataFrame,
-    model: str,
-    token_budget: int
-) -> str:
+
+
+def query_message(query: str, df: pd.DataFrame, model: str, token_budget: int) -> str:
     """Return a message for GPT, with relevant source texts pulled from a dataframe."""
-    print("Querying message")
     strings, relatedness = strings_ranked_by_relatedness(query, df, top_n=10)
+    print("Querying message")
     introduction = "Use the below clues and their respective answers to help you solve the subsequent clue."
     question = f"\n\n{query}"
     message = introduction
     for string in strings:
         next_clue_set = f'\n\nClue Set:"""\n{string}\n"""'
-        if (num_tokens(message + next_clue_set + question, model=model) > token_budget):
+        if num_tokens(message + next_clue_set + question, model=model) > token_budget:
             print("WARNING: Length of clue sets exceeded token budget.")
             break
         else:
             message += next_clue_set
     return message + question
-        
-        
+
+
 def ask(
     query: str,
     df: pd.DataFrame,
     model: str = GPT_MODEL,
     token_budget: int = 4096 - 500,
-    print_message: bool = False
+    print_message: bool = False,
 ) -> str:
     """Answers a query using GPT and a dataframe of relevant texts and embeddings."""
-    print("Asking question")
-    # message = query_message(query, df, model=model, token_budget=token_budget)
-    # if print_message:
-    #     print(message)
+    message = query_message(query, df, model=model, token_budget=token_budget)
+    if print_message:
+        print(message)
     messages = [
-        {"role": "system", 
-         "content": """You are an expert crossword solver. When given a crossword clue and the length 
+        {
+            "role": "system",
+            "content": """You are an expert crossword solver. When given a crossword clue and the length 
                        of the clue's answer, you briefly give your FIVE best guesses for the clue. Your 
-                       guesses MUST be the length of the answer and should be in all caps."""},
-        {"role": "user", "content": query}
+                       guesses MUST be the length of the answer and should be in all caps.""",
+        },
+        {"role": "user", "content": message},
     ]
+    print("Asking question")
     response = openai.chat.completions.create(
-        model=model,
-        messages=messages,                                                                                  # type: ignore
-        temperature=0.4
+        model=model, messages=messages, temperature=0.4  # type: ignore
     )
     response_message = response.choices[0].message.content
-    return response_message                                                                                 # type: ignore
-     
+    return response_message  # type: ignore
+
+
 def try_literal_eval(row):
     try:
         return ast.literal_eval(row)
     except BaseException:
         pass
-        
+
+
 def main() -> None:
-    ddf = dd.read_csv(SAVE_PATH, blocksize="25MB",                                                          # type: ignore
-                                 dtype={"text": str},
-                                 engine="python",
-                                 on_bad_lines="warn")
-    ddf["embedding"] = ddf["embedding"].apply(try_literal_eval, meta=('embedding', 'object'))
+    ddf = dd.read_csv(  # type: ignore
+        SAVE_PATH,
+        dtype={"text": str},
+        engine="python",
+        on_bad_lines="warn",
+    )
+    ddf["embedding"] = ddf["embedding"].apply(
+        try_literal_eval, meta=("embedding", "object")
+    )
     ddf = ddf.dropna()
     # example question
-    question = """Metric weight units (6 Letters)"""
-    repsonse = ask(question, ddf)
+    question = """Paper size longer than letter (5 Letters)"""
+    repsonse = ask(question, ddf, print_message=True)
     print(repsonse)
-    
+
+
 if __name__ == "__main__":
     main()
