1882abca99355bafab9c76da2b9eb432ba737fb5
E Harrison <ehharrison13@gmail.com>
2023-12-22 17:19:00 -0800

    matrix optimization and results!

--- embedding_customizer.py
+++ embedding_customizer.py
@@ -1,4 +1,3 @@
-from enum import unique
 import numpy as np
 import pandas as pd
 import plotly.express as px
@@ -13,7 +12,7 @@ from embeddings_util import get_embedding, cosine_similarity
 embedding_cache_path = "data/clueqa_embedding_cache.pkl"  # embeddings will be saved/loaded here
 default_embedding_engine = "text-embedding-ada-002"  # text-embedding-ada-002 is recommended
 local_dataset_path = "data/unique_qa_pairs.csv" 
-num_pairs_to_embed = 1000  # 1000 is arbitrary
+num_pairs_to_embed = 5000  # 1000 is arbitrary
 random_seed = 1987
 
 def process_input_data(df: pd.DataFrame) -> pd.DataFrame:
@@ -66,6 +65,172 @@ def accuracy_and_se(cosine_similarity, labeled_similarity) -> tuple[float, float
     standard_error = (a * (1 - a) / n) ** 0.5  # standard error of binomial
     return a, standard_error
 
+def embedding_multiplied_by_matrix(
+        embedding: list[float], matrix: torch.Tensor
+    ) -> np.ndarray:
+        embedding_tensor = torch.tensor(embedding).float()
+        modified_embedding = embedding_tensor @ matrix
+        modified_embedding = modified_embedding.detach().numpy()
+        return modified_embedding
+    
+# compute custom embeddings and new cosine similarities
+def apply_matrix_to_embeddings_dataframe(matrix: torch.Tensor, df: pd.DataFrame):
+    for column in ["text_1_embedding", "text_2_embedding"]:
+        df[f"{column}_custom"] = df[column].apply(
+            lambda x: embedding_multiplied_by_matrix(x, matrix) # type: ignore
+        ) # type: ignore
+    df["cosine_similarity_custom"] = df.apply(
+        lambda row: cosine_similarity(
+            row["text_1_embedding_custom"], row["text_2_embedding_custom"]
+        ),
+        axis=1,
+    )
+    
+def optimize_matrix(
+    df: pd.DataFrame,
+    modified_embedding_length: int = 1536,  # in my brief experimentation, bigger was better (2048 is length of babbage encoding)
+    batch_size: int = 100,
+    max_epochs: int = 100,
+    learning_rate: float = 100.0,  # seemed to work best when similar to batch size - feel free to try a range of values
+    dropout_fraction: float = 0.0,  # in my testing, dropout helped by a couple percentage points (definitely not necessary)
+    print_progress: bool = True,
+    save_results: bool = True,
+) -> torch.Tensor:
+    """Return matrix optimized to minimize loss on training data."""
+    run_id = random.randint(0, 2 ** 31 - 1)  # (range is arbitrary)
+    # convert from dataframe to torch tensors
+    # e is for embedding, s for similarity label
+    def tensors_from_dataframe(
+        df: pd.DataFrame,
+        embedding_column_1: str,
+        embedding_column_2: str,
+        similarity_label_column: str,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        e1 = np.stack(np.array(df[embedding_column_1].values)) # type: ignore
+        e2 = np.stack(np.array(df[embedding_column_2].values)) # type: ignore
+        s = np.stack(np.array(df[similarity_label_column].astype("float").values)) # type: ignore
+
+        e1 = torch.from_numpy(e1).float()
+        e2 = torch.from_numpy(e2).float()
+        s = torch.from_numpy(s).float()
+
+        return e1, e2, s
+    
+    e1_train, e2_train, s_train = tensors_from_dataframe(
+        df[df["dataset"] == "train"], "text_1_embedding", "text_2_embedding", "label"
+    )
+    e1_test, e2_test, s_test = tensors_from_dataframe(
+        df[df["dataset"] == "test"], "text_1_embedding", "text_2_embedding", "label"
+    )
+
+    # create dataset and loader
+    dataset = torch.utils.data.TensorDataset(e1_train, e2_train, s_train)
+    train_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=True
+    )
+    
+    # define model (similarity of projected embeddings)
+    def model(embedding_1, embedding_2, matrix, dropout_fraction=dropout_fraction):
+        e1 = torch.nn.functional.dropout(embedding_1, p=dropout_fraction)
+        e2 = torch.nn.functional.dropout(embedding_2, p=dropout_fraction)
+        modified_embedding_1 = e1 @ matrix  # @ is matrix multiplication
+        modified_embedding_2 = e2 @ matrix
+        similarity = torch.nn.functional.cosine_similarity(
+            modified_embedding_1, modified_embedding_2
+        )
+        return similarity
+
+    # define loss function to minimize
+    def mse_loss(predictions, targets):
+        difference = predictions - targets
+        return torch.sum(difference * difference) / difference.numel()
+
+    # initialize projection matrix
+    embedding_length = len(df["text_1_embedding"].values[0])
+    matrix = torch.randn(
+        embedding_length, modified_embedding_length, requires_grad=True
+    )
+    
+    epochs, types, losses, accuracies, matrices = [], [], [], [], []
+    for epoch in range(1, 1 + max_epochs):
+        # iterate through training dataloader
+        for a, b, actual_similarity in train_loader:
+            # generate prediction
+            predicted_similarity = model(a, b, matrix)
+            # get loss and perform backpropagation
+            loss = mse_loss(predicted_similarity, actual_similarity)
+            loss.backward()
+            # update the weights
+            with torch.no_grad():
+                matrix -= matrix.grad * learning_rate # type: ignore
+                # set gradients to zero
+                matrix.grad.zero_()
+        # calculate test loss
+        test_predictions = model(e1_test, e2_test, matrix)
+        test_loss = mse_loss(test_predictions, s_test)
+
+        # compute custom embeddings and new cosine similarities
+        apply_matrix_to_embeddings_dataframe(matrix, df)
+
+        # calculate test accuracy
+        for dataset in ["train", "test"]:
+            data = df[df["dataset"] == dataset]
+            a, se = accuracy_and_se(data["cosine_similarity_custom"], data["label"])
+
+            # record results of each epoch
+            epochs.append(epoch)
+            types.append(dataset)
+            losses.append(loss.item() if dataset == "train" else test_loss.item()) # type: ignore
+            accuracies.append(a)
+            matrices.append(matrix.detach().numpy())
+
+            # optionally print accuracies
+            if print_progress is True:
+                print(
+                    f"Epoch {epoch}/{max_epochs}: {dataset} accuracy: {a:0.1%} ± {1.96 * se:0.1%}"
+                )
+                
+    data = pd.DataFrame(
+        {"epoch": epochs, "type": types, "loss": losses, "accuracy": accuracies}
+    )
+    data["run_id"] = run_id
+    data["modified_embedding_length"] = modified_embedding_length
+    data["batch_size"] = batch_size
+    data["max_epochs"] = max_epochs
+    data["learning_rate"] = learning_rate
+    data["dropout_fraction"] = dropout_fraction
+    data[
+        "matrix"
+    ] = matrices  # saving every single matrix can get big; feel free to delete/change
+    if save_results is True:
+        data.to_csv(f"{run_id}_optimization_results.csv", index=False)
+
+    return data # type: ignore
+
+def plot_hyperparameter_training(runs_df: pd.DataFrame, measure_loss: bool = True):
+    y = "loss" if measure_loss else "accuracy"
+    px.line(
+        runs_df,
+        line_group="run_id",
+        x="epoch",
+        y=y,
+        color="type",
+        hover_data=["batch_size", "learning_rate", "dropout_fraction"],
+        facet_row="learning_rate",
+        facet_col="batch_size",
+        width=500,
+    ).show()
+
+def plot_cosine_similarity_histogram(df: pd.DataFrame, custom: bool=True):
+    x = "cosine_similarity_custom" if custom else "cosine_similarity"
+    px.histogram(
+        df,
+        x=x,
+        color="label",
+        barmode="overlay",
+        width=500,
+        facet_row="dataset",
+    ).show()
 
 def main():
     # process data
@@ -134,21 +299,49 @@ def main():
         lambda row: cosine_similarity(row["text_1_embedding"], row["text_2_embedding"]),
         axis = 1
     )
-    
-    # check that training and test sets are balanced
-    px.histogram(
-        df,
-        x="cosine_similarity",
-        color="label",
-        barmode="overlay",
-        width=500,
-        facet_row="dataset",
-    ).show()
+        
+    # example hyperparameter search
+    # I recommend starting with max_epochs=10 while initially exploring
+    results = []
+    max_epochs = 30
+    dropout_fraction = 0.2
+    for batch_size, learning_rate in [(1000, 1000), (2000, 2000), (3000, 3000)]:
+        result = optimize_matrix(
+            df=df,
+            batch_size=batch_size,
+            learning_rate=learning_rate,
+            max_epochs=max_epochs,
+            dropout_fraction=dropout_fraction,
+            save_results=False,
+        )
+        results.append(result)
+        
+    runs_df = pd.concat(results)
 
-    for dataset in ["train", "test"]:
-        data = df[df["dataset"] == dataset]
-        a, se = accuracy_and_se(data["cosine_similarity"], data["label"])
-        print(f"{dataset} accuracy: {a:0.1%} ± {1.96 * se:0.1%}")
+
+    # plot training loss and test loss over time
+    plot_hyperparameter_training(runs_df, measure_loss=True)
+
+    # plot accuracy over time
+    plot_hyperparameter_training(runs_df, measure_loss=False)
+    
+    # apply result of best run to original data
+    best_run = runs_df.sort_values(by="accuracy", ascending=False).iloc[0]
+    best_matrix = best_run["matrix"]
+    apply_matrix_to_embeddings_dataframe(best_matrix, df)
+    
+    # plot similarity distribution BEFORE customization
+    plot_cosine_similarity_histogram(df, custom=False)
+
+    test_df = df[df["dataset"] == "test"]
+    a, se = accuracy_and_se(test_df["cosine_similarity"], test_df["label"])
+    print(f"Test accuracy: {a:0.1%} ± {1.96 * se:0.1%}")
+
+    # plot similarity distribution AFTER customization
+    plot_cosine_similarity_histogram(df, custom=True)
+
+    a, se = accuracy_and_se(test_df["cosine_similarity_custom"], test_df["label"])
+    print(f"Test accuracy after customization: {a:0.1%} ± {1.96 * se:0.1%}")
 
 if __name__ == "__main__":
     main()
